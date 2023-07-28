394345db2a8ed6dc54d244e6f98c0c76b7982ab1
Desiree Zarco <desireeannzarco7@gmail.com>
2023-07-28 14:16:17 -0700

    completed deliverables

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 // use these variables in your JSX (update them with your own name and city!)
-const name = "Liza";
-const city = "New York";
+const name = "Desiree";
+const city = "Las Vegas";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
